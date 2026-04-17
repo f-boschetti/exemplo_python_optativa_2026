292529839657ed3a187c60903e0cfeba2c8595d2
f-boschetti <felipeboss232@gmail.com>
2026-04-16 23:40:07 -0300

    Fim da aula

--- novosite/views.py
+++ novosite/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from novosite.forms import CarroForm
 from novosite.models import Carro
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
     data = {}
-    
-    data['carros'] = Carro.objects.filter(excluido=False)
+    all_carros = Carro.objects.filter(excluido=False).order_by('id')
+    paginator = Paginator(all_carros, 2)
+    page = request.GET.get('page')
+    data['carros'] = paginator.get_page(page)
     return render(request, 'index.html', data)
 
 def form(request):
